a0309be12c84e5ba589087150951f5e1388415b5
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-07-10 04:24:28 -0400

    Adding table properties

--- server/models.py
+++ server/models.py
@@ -1,6 +1,76 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
 
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    password_hash = db.Column(db.string)
+
+# Add User relationship
+
+
+# Add serialization rules
+
+
+# Add validation
+
+
+class Order(db.Model, SerializerMixin):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.Timestamp)
+    status = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+# Add Order relationship
+
+
+# Add serialization rules
+
+
+# Add validation
+
+
+class OrderItem(db.Model, SerializerMixin):
+    __tablename__ = "order_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Numeric)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+
+# Add OrderItem relationship
+
+
+# Add serialization rules
+
+
+# Add validation
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Integer)
+    description = db.Column(db.String)
+    price = db.Column(db.Numeric)
+    image_url = db.Column(db.String)
+    available = db.Column(db.Boolean)
+    size = db.Column(db.String)
+
+# Add Product relationship
+
+
+# Add
