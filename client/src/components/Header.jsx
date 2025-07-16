3effcf4c54ea25ca4197cd2bdf5e1793d2762fee
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-07-16 18:42:31 -0400

    Added some frontend designs

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <h1>Sweet Ice Delights</h1>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <main className="home">
+      <h1>Welcome to Sweet Ice Delights</h1>
+      <p>Your content goes here</p>
+    </main>
+  )
+}
+
+export default Home
